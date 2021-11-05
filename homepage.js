92419b640b753a10112a46c80ba01ac48358672b
Nagaveena007 <nagaveena.hallikeri@gmail.com>
2021-11-05 17:57:57 +0100

    adds files

---
+++ homepage.js
@@ -0,0 +1,6 @@
+/* fetch("https://striveschool-api.herokuapp.com/api/put-your-endpoint-here/", {
+headers: {
+"Authorization": "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2MTgyODk0ZGFhY2FhMjAwMTU1MmExNjMiLCJpYXQiOjE2MzYxMTgwNDcsImV4cCI6MTYzNzMyNzY0N30.7idrIDQ6__QkkjU5hOTvdBSoLnymYHRZSu3S-eOwSZo"
+}
+}
+ */
